0755694f731335afd3ccea23759e6714b0f04105
Akhil Francis <akhilfrancis0007@gmail.com>
2024-04-08 22:12:08 +0530

    Added Env Variables  Validation Before App Initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AccessKeyModule } from './access-key/access.key.module';
 import typeorm from './config/typeorm';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [typeorm],
+      validationSchema: Joi.object({
+        PGHOST: Joi.string().required(),
+        PGDATABASE: Joi.string().required(),
+        PGUSER: Joi.string().required(),
+        PGPORT: Joi.number().required(),
+        PGPASSWORD: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
